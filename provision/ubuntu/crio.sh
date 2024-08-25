4b1ec0e9ca55a0140207d9c7e6deacc79bc4c00c
NxPKG <iconmamundentist@gmail.com>
2024-08-26 03:03:41 +0600

    Create crio.sh
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ provision/ubuntu/crio.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+source "${ENV_FILEPATH}"
+
+set -e
+
+OS=xUbuntu_22.04
+VERSION=1.24
+echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /" > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
+
+curl -L https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable:cri-o:$VERSION/$OS/Release.key | apt-key add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key | apt-key add -
+
+apt-get update
+apt-get install -y cri-o cri-o-runc
