5a32888daaa827f24e52eab825ccba5d016f446a
NxPKG <iconmamundentist@gmail.com>
2024-08-26 03:14:25 +0600

    Create env.bash
    Signed-off-by: NxPKG <iconmamundentist@gmail.com>

---
+++ provision/env.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+export GOLANG_VERSION="1.20.1"
+export ETCD_VERSION="v3.1.0"
+export CONTAINERD_VERSION="1.6.4"
+export SONOBUOY_VERSION="0.14.2"
+export PROTOC_VERSION="3.12.4"
+export HOME_DIR=/home/vagrant
+export HOME=/home/vagrant
+export GOPATH="${HOME}/go"
+export PATH="${GOPATH}/bin:$PATH"
